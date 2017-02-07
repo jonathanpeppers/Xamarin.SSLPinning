32f30470f55a83e091baf41374bdcbe5396612fa
Jonathan Peppers <jonathan.peppers@hitcents.com>
2017-02-06 21:40:41 -0600

    iOS Test project

---
+++ Xamarin.SSLPinning.iOS/HttpsTest.cs
@@ -0,0 +1,20 @@
+﻿using System.Net.Http;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace Xamarin.SSLPinning.iOS
+{
+    [TestFixture]
+    public class HttpsTest
+    {
+        private const int Timeout = 3000;
+
+        [Test, Timeout(Timeout)]
+        public async Task Get()
+        {
+            var httpClient = new HttpClient(new Foundation.NSUrlSessionHandler());
+            var response = await httpClient.GetAsync("https://httpbin.org/get");
+            response.EnsureSuccessStatusCode();
+        }
+    }
+}
